f601cc8b1f49a383b36a86d524df952cf4802b64
Kazuki Sato <kazuki.sato.japan@gmail.com>
2018-10-29 20:39:59 +0900

    mod

--- simplewebtools/config.py
+++ simplewebtools/config.py
@@ -1,6 +1,7 @@
 # testing environment
 # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:mysqlpass@localhost/swt?charset=utf8'
 # production environment
-SQLALCHEMY_DATABASE_URI = 'mysql://bb4b0b0283f8fe:ca310436@us-cdbr-iron-east-01.cleardb.net/heroku_b078ee8a5ee4f2a?reconnect=true'
+# SQLALCHEMY_DATABASE_URI = 'mysql://bb4b0b0283f8fe:ca310436@us-cdbr-iron-east-01.cleardb.net/heroku_b078ee8a5ee4f2a?reconnect=true'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://bb4b0b0283f8fe:ca310436@us-cdbr-iron-east-01.cleardb.net/heroku_b078ee8a5ee4f2a?reconnect=true'
 SECRET_KEY = 'secret key'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
